1bde43e2a98e70418bcc96d3735e90925fddba38
darac001 <114274317+darac001@users.noreply.github.com>
2023-01-17 11:12:22 -0700

    Add files via upload

---
+++ 01_Expanding cards/script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    // console.log('clicked')
+    removeActive();
+    panel.classList.add('active');
+  })
+}
+)
+
+
+function removeActive () {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  })
+};
